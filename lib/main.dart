712153d50d6b226a0eba0856a722fc5b8ca3dbab
AdrianCM21 <adriancasco2002@gmail.com>
2024-04-20 15:09:54 -0400

    Mi primer commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:rodados_admin_app/src/routes/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(routes: routes);
+  }
+}
